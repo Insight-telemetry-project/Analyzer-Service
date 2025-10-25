6f0718a2682c68382677f5896de8350d7a11d60a
nadav5 <nadavbar205@gmail.com>
2025-10-25 10:23:51 +0300

    [FEATURE] Implement CCM Analysis algorithm

---
+++ Analyzer Service/Models/Interface/Algorithms/Ccm/ICcmCausalityAnalyzer.cs
@@ -0,0 +1,7 @@
+﻿namespace Analyzer_Service.Models.Interface.Algorithms.Ccm
+{
+    public interface ICcmCausalityAnalyzer
+    {
+        double ComputeCausality(List<double> xSeries, List<double> ySeries, int embeddingDim, int delay);
+    }
+}
